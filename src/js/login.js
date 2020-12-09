f7d9f2a267feb4b3c12fb118b0f2c8465435469b
kebi_ko <kebi3477@naver.com>
2020-12-09 19:54:38 +0900

    Change SignUp Event

--- src/js/login.js
+++ src/js/login.js
@@ -74,10 +74,9 @@ class login extends Component {
                     "Content-Type": "application/json; charset=utf-8"
                 },
                 body: JSON.stringify(user)
-            }).then(() => {
-                alert('회원 등록 되었습니다!');
-                this.state.setState({ onSignUp : false })
             })
+            alert('회원 등록 되었습니다!');
+            this.setState({ onSignUp : false })
         }
     }
 
